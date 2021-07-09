204b033e3a9ccd4068a38f89d9a10eef5383a835
YMG <y.munizgonzalez@gmail.com>
2021-07-09 18:23:43 -0300

    input

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -37,14 +37,14 @@ function Home() {
   };
 
   useEffect(() => {
-    document.getElementById("phone").addEventListener("blur", function (e) {
-      const x = e.target.value
-        .replace(/\D/g, "")
-        .match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
-      e.target.value = !x[2]
-        ? x[1]
-        : `(${x[1]}) ${x[2]}${x[3] ? `-${x[3]}` : ""}`;
-    });
+    // document.getElementById("phone").addEventListener("blur", function (e) {
+    //   const x = e.target.value
+    //     .replace(/\D/g, "")
+    //     .match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+    //   e.target.value = !x[2]
+    //     ? x[1]
+    //     : `(${x[1]}) ${x[2]}${x[3] ? `-${x[3]}` : ""}`;
+    // });
 
     if (window.location.hash === "#register") {
       executeScroll();
